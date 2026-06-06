f2b5aa4b5c8352a38c599507adf3db8191b46685
Dheerajkumar <dheerajkumar9877k@gmail.com>
2026-06-06 22:13:17 +0530

    functon addItem

--- logic.js
+++ logic.js
@@ -1,1 +1,15 @@
+function addItem(){
+    var task = document.getElementById('taskInput').value;
+    var list = document.querySelector('.list');
+    var total = document.querySelector('#all');
+    var pending = document.querySelector('#pending');
+    
+    if(task.trim() != ''){
+      list.innerHTML += `<input type="checkbox" onclick="deletTask()">${task}</input>`; 
+      total.innerHTML = Number(total.innerText)+1;
+      pending.innerHTML = Number(pending.innerText)+1;
+    } else {
+        alert('Please enter a valid task value!');
+    }
+}
 
